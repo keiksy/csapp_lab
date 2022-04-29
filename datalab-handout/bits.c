4307430e38648846e34c316dbe584aa03e3b0c63
CXY@dorm <hsinyaotsai@foxmail.com>
2022-04-29 17:05:51 +0800

    bomb

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ~(~x&~y)&~(x&y);
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +152,7 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  return 1 << 31;
 }
 //2
 /*
@@ -165,7 +163,9 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  int a = x+1;
+  
+  return ;
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
